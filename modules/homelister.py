87233442b895a7769adac4a51ba12801bfa571b8
Spiros Avlonitis <spirosa84@hotmail.com>
2019-01-05 06:52:39 +0200

    os.name provision

--- modules/homelister.py
+++ modules/homelister.py
@@ -4,4 +4,6 @@ import getpass
 def run(**args):
     """List home dir"""
     print("[*] In homelister module.")
+    if "nt" in os.name:
+        return str(os.listdir("C:\\"))
     return str(os.listdir(f"/home/{getpass.getuser()}"))
